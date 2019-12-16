5f22b8206b59bcf48ba2f80d8c8568300670dffb
w8lt8r <victor8titov@ya.ru>
2019-12-16 18:27:15 +0300

    сжатие удаление подготовка к продакшн

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-
-function ms($m) {
-    echo "-----\n";
-    echo'<pre>';
-    var_dump($m);
-    echo '</pre>';
-    echo "-----\n";
-}
-
-
 ini_set('display_errors', 1);
 require_once 'application/bootstrap.php';
